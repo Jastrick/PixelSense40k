248c2535ed4f77a75659e9abd7b4db3a2a375c21
Kyle Yasinski <jastrick.mazziki@gmail.com>
2013-09-18 11:33:35 -0600

    Initial push

---
+++ PixelSense40k/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PixelSense40k
+{
+    public class Infantry : Unit
+    {
+        public Infantry()
+        {
+            
+        }
+    }
+}
---
+++ PixelSense40k/Unit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Surface.Presentation.Input;
+
+public class Unit
+{
+    private TagData unitID;
+    private int maxMove;
+    private String name;
+    private int faction;
+    private int WS, BS, S, T, W, I, A, Ld, Sv, SvType;
+
+    public Unit()
+	{
+	}
+}
