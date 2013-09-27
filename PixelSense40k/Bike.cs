b4c963cd079952b4165ce872863d7c524a75c62e
Kyle Yasinski <jastrick.mazziki@gmail.com>
2013-09-27 11:35:08 -0600

    Completed model recognition. Pixelsense can now recognize all 49 models.

---
+++ PixelSense40k/Bike.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Surface.Presentation.Input;
+
+namespace PixelSense40k
+{
+    public class Bike : Unit
+    {
+        public Bike(String n, int t)
+        {
+            Name = n;
+            UnitID = t;
+            MaxMove = 12;
+        }
+    }
+}
